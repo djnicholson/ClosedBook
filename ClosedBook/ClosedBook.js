08ca628e6b05e5495d01fd67e942ddfb4682a02b
djnicholson <me@djnicholson.com>
2015-10-02 13:53:50 -0700

    get the right permissions

--- ClosedBook/ClosedBook.js
+++ ClosedBook/ClosedBook.js
@@ -1,33 +1,37 @@
 var appId = 559895940818205; // TODO: Allow easy customization
+var requiredPermissions = "user_posts";
 
-var login = function (currentStatus) {
+var startLogin = function () {
+    FB.login(processLoginResponse, { scope: requiredPermissions });
+};
+
+var processLoginResponse = function (currentStatus) {
     if (currentStatus.status !== "connected") {
         // not signed into FB (or app not authorized)
-        alert("not logged in or authorized: " + currentStatus.status);
+        alert("You are not logged into Facebook, or have not authorized this app to view your posts (\"" +
+            currentStatus.status + "\") please login.");
+        startLogin();
     } else {
-        alert("api");
         // Get a list of all posts by active user:
         FB.api(
-          '/me',
-          'GET',
-          { "fields": "posts{id}" },
-          function (response) {
-              alert(JSON.stringify(response));
-          }
+            '/me',
+            'GET',
+            { "fields": "posts{id}" },
+            function (response) {
+                alert(JSON.stringify(response));
+            }
         );
     }
 };
 
 $(document).ready(function () {
 
-    alert("init");
     FB.init({
         appId: appId,
         xfbml: true,
         version: 'v2.4',
     });
 
-    alert("login");
-    FB.getLoginStatus(login);
+    startLogin();
 });
 
