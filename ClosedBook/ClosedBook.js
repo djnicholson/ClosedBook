d938b5558833a4050408b66616c3193aff324eb9
djnicholson <me@djnicholson.com>
2015-10-02 13:37:08 -0700

    working API call

--- ClosedBook/ClosedBook.js
+++ ClosedBook/ClosedBook.js
@@ -1,11 +1,33 @@
-// TODO: Init FB API
+var appId = 559895940818205; // TODO: Allow easy customization
 
-// Get a list of all posts by active user:
-FB.api(
-  '/me',
-  'GET',
-  { "fields": "posts{id}" },
-  function (response) {
-      alert(JSON.stringify(response));
-  }
-);
+var login = function (currentStatus) {
+    if (currentStatus.status !== "connected") {
+        // not signed into FB (or app not authorized)
+        alert("not logged in or authorized: " + currentStatus.status);
+    } else {
+        alert("api");
+        // Get a list of all posts by active user:
+        FB.api(
+          '/me',
+          'GET',
+          { "fields": "posts{id}" },
+          function (response) {
+              alert(JSON.stringify(response));
+          }
+        );
+    }
+};
+
+$(document).ready(function () {
+
+    alert("init");
+    FB.init({
+        appId: appId,
+        xfbml: true,
+        version: 'v2.4',
+    });
+
+    alert("login");
+    FB.getLoginStatus(login);
+});
+
