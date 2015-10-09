c1cc4c9e54135f4f0a1c9f4d242c62dcb7411a0d
David Nicholson <me@djnicholson.com>
2015-10-09 16:58:51 -0700

    Note about Python requirement

--- ClosedBook/RunHTTPServer.cmd
+++ ClosedBook/RunHTTPServer.cmd
@@ -1,6 +1,16 @@
 echo off
 cls
+
+echo.
+echo NOTE: This script requires Python to be installed and available in your PATH.
+echo Go to www.python.org to install.
+echo.
+
 echo Running a HTTP server in:
 cd
+echo.
+
 python -m http.server --cgi 8000
+
+echo.
 pause
